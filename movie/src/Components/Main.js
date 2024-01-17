a9b5a4fa00ef07335f259900a7bff8f3462c85e3
ereza <erzaamerovci@gmail.com>
2024-01-17 14:55:14 +0100

    Started designing the header

---
+++ movie/src/Components/Main.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <>
+      <div className="header">
+        <nav>
+            <ul>
+            <li>
+            <a href="#">Popular</a>
+          </li>
+          <li>
+            <a href="#">Theatre</a>
+          </li>
+          <li>
+            <a href="#">Kids</a>
+          </li>
+          <li>
+            <a href="#">Drama</a>
+          </li>
+          <li>
+            <a href="#">Comedy</a>
+          </li>
+            </ul>
+         
+        </nav>
+        <form>
+            <div className="search-btn">
+                <input type="text" placeholder="Enter movie name" className="inputText"></input>
+                <button><i class="fas fa-search"></i></button>
+
+            </div>
+        </form>
+      </div>
+    </>
+  );
+};
