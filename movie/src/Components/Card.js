eff6bd5d94ce14a9f3888315248d17e1a1665745
ereza <erzaamerovci@gmail.com>
2024-01-19 13:27:04 +0100

    added the release date in the card

--- movie/src/Components/Card.js
+++ movie/src/Components/Card.js
@@ -1,24 +1,30 @@
-import react from "react";
+import React from "react";
 
-const Card=(movie)=>{
-    console.log(movie.info);
-    let img_path = "https://image.tmdb.org/t/p/w500";
-    return(
-        <>
-        <div className="movie">
-            <img className="poster" src={img_path+movie.info.poster_path}></img>
-            <div className="movie-details">
-                <div className="box">
-                    <h4 className="movie-title"> {movie.info.title}</h4>
-                    <p className="rating">{movie.info.vote_average}</p>
-                </div>
-                <div className="overview"> {movie.info.overview}</div>
-            </div>
+const Card = ({ info }) => {
+  const img_path = "https://image.tmdb.org/t/p/w500";
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+  };
+
+  return (
+    <>
+      <div className="movie">
+        <img className="poster" src={img_path + info.poster_path} alt={info.title} />
+        <div className="movie-details">
+          <div className="box">
+            <h4 className="movie-title">{info.title}</h4>
+            <p className="rating">{info.vote_average}</p>
+          </div>
+          <div className="overview">
+            <p className="release-date">Release Date: {formatDate(info.release_date)}</p>
+            <p>{info.overview}</p>
+          </div>
         </div>
-      
-      
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Card;
